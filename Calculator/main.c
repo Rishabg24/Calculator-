ab926f03acd57221c9469add25dd580835fb79f5
Programmer.Lotus <77867932+Rishabg24@users.noreply.github.com>
2022-12-31 17:07:27 -0800

    Update main.c

--- Calculator/main.c
+++ Calculator/main.c
@@ -1,72 +1,85 @@
 
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
 // Function prototypes
-double add(double a, double b);
-double subtract(double a, double b);
-double multiply(double a, double b);
-double divide(double a, double b);
+double Add(double a, double b);
+double Subtract(double a, double b);
+double Multiply(double a, double b);
+double Divide(double a, double b);
+double Exponent(double a, double b);
+
 
 int main()
 {
-    // Declare variables for the operands and the result
-    double operand1, operand2, result;
-    
+     // Declare variables 
+    double  operand1, operand2, result, valid;
+    char operation;
+
     // Prompt the user for the operands
     printf("Enter the first operand: ");
     scanf("%lf", &operand1);
-    
     printf("Enter the second operand: ");
     scanf("%lf", &operand2);
     
     // Prompt the user for the operation
-    printf("Enter the operation (+, -, *, /): ");
-    char operation;
+    printf("Enter the operation (+, -, *, /, ^): ");
     scanf(" %c", &operation);
+
     
     // Perform the operation and store the result
     switch (operation)
     {
         case '+':
-            result = add(operand1, operand2);
+            result = Add(operand1, operand2);
             break;
         case '-':
-            result = subtract(operand1, operand2);
+            result = Subtract(operand1, operand2);
             break;
         case '*':
-            result = multiply(operand1, operand2);
+            result = Multiply(operand1, operand2);
             break;
         case '/':
-            result = divide(operand1, operand2);
+            result = Divide(operand1, operand2);
+            break;
+        case '^':
+            result = Exponent(operand1, operand2);
             break;
         default:
             printf("Invalid operation.\n");
             return 1;
+            break;
     }
     
     // Print the result
     printf("The result is: %lf\n", result);
     
-    return 0;
 }
 
+
 // Function definitions
-double add(double a, double b)
+double Add(double a, double b)
 {
     return a + b;
 }
 
-double subtract(double a, double b)
+double Subtract(double a, double b)
 {
     return a - b;
 }
 
-double multiply(double a, double b)
+double Multiply(double a, double b)
 {
     return a * b;
 }
 
-double divide(double a, double b)
+double Divide(double a, double b)
 {
     return a / b;
+}
+
+double Exponent(double a, double b)
+{
+    return pow(a,b);
 }
